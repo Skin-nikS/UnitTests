3ad5dd62d0e0b9f520eb0f3d31d8fbad607430f6
FailX <xaibullin.96@mail.ru>
2023-04-05 19:26:09 +0300

    com

---
+++ UnitTests.Tests/CalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using UnitTests;
+
+namespace UnitTests.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact] 
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            calculator.Miltiplication(2, 4);
+
+            Assert.Equal(8, calculator.Miltiplication(2,4));
+
+            calculator.Subtraction(4, 2);
+            Assert.Equal(2, calculator.Subtraction(4,2));
+
+
+            calculator.Additional(4, 2);
+            Assert.Equal(6, calculator.Additional(4, 2));
+
+
+            calculator.Division(4, 2);
+            Assert.Equal(2, calculator.Division(4, 2));
+
+        }
+    }
+}
--- UnitTests/Program.cs
+++ UnitTests/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Practices
+{
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
